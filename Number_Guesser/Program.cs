ee19a41a5d81b92c676355cbde51918ada649934
Bianca <biancadev4@gmail.com>
2025-02-01 16:45:31 +0200

    Created and added Outline
    I created the Project and added the Start of the game Including the header,Asking for the users name and an If statement verifying whether the user wants to play or not.

---
+++ Number_Guesser/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Number_Guesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appName ="Number Guessing Game!";
+            string appVersion ="2.0";
+            string appAuthor = "Me :)";
+
+            //COLOUR
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            Console.WriteLine("{0} Version {1} by {2}",appName,appVersion,appAuthor);
+            Console.ResetColor();
+
+            Console.WriteLine("Your Name: ");
+            string userName = Console.ReadLine();
+            
+
+            //Maybe use a dowhile?
+
+            Console.WriteLine("Hello {0} ,Welcome to THE NUMBER GUESSING GAME! \nDo you want to Play?(Y/N)" ,userName);
+            string wannaPlay = Console.ReadLine();
+
+            if(wannaPlay == "Y"){
+                //play the game
+            }else if(wannaPlay=="N"){
+                //exit
+            }else{
+                //wrong input
+            }
+
+
+            //Keeps Console Open while testing
+            Console.ReadKey();
+
+
+        }
+    }
+}
